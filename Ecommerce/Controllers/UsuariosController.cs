85931e630f85eadeac346fb89b357f45e07ca5e9
Diego008 <diegoaugusto008@gmail.com>
2017-05-31 01:45:31 -0300

    Serialize

--- Ecommerce/Controllers/UsuariosController.cs
+++ Ecommerce/Controllers/UsuariosController.cs
@@ -5,49 +5,35 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Ecommerce.Models;
+using Newtonsoft.Json;
 
 namespace Ecommerce.Controllers
 {
     public class UsuariosController : Controller
     {
         private Context db = new Context();
-        IList<string> contatos;
+        
         // GET: Usuarios
         public ActionResult Create()
         {
             return View();
         }
-        
-        [HttpPost]
-        public IList<string> SaveContact(string[] todosContatos)
-        {
-            contatos = new List<string>(todosContatos.Length);
-
-            foreach (string itens in todosContatos)
-            {
-                contatos.Add(itens);
-            }
-
-            return contatos;
-        }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "UsuarioID,Nome,Cpf,Rg,Cep,Endereco,Numero,Bairro,Cidade,Contato,Email,Senha,Imagem")] Usuario usuario)
+        public ActionResult Create(string user)
         {
-            foreach (string item in contatos)
-            {
-                usuario.Contato += item + ", ";
-            }
+            //string json = JsonConvert.SerializeObject(user);
 
+            Usuario deserializeUsuario = JsonConvert.DeserializeObject<Usuario>(user);
             if (ModelState.IsValid)
             {
-                db.Usuarios.Add(usuario);
-                db.SaveChanges();                
+                db.Usuarios.Add(deserializeUsuario);
+                db.SaveChanges();
                 return RedirectToAction("Create");
             }
 
-            return View(usuario);
+            return View(deserializeUsuario);
         }
     }
 }
