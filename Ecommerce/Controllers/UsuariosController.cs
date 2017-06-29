5214a6d6534d24d19e87d58eb89a7702b22180e0
Diego008 <diegoaugusto008@gmail.com>
2017-06-29 14:29:12 -0300

    atualizando cadastro de usuario

--- Ecommerce/Controllers/UsuariosController.cs
+++ Ecommerce/Controllers/UsuariosController.cs
@@ -12,7 +12,7 @@ namespace Ecommerce.Controllers
     public class UsuariosController : Controller
     {
         private Context db = new Context();
-        
+
         // GET: Usuarios
         public ActionResult Create()
         {
@@ -21,13 +21,33 @@ namespace Ecommerce.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Usuario usuario)
+        public ActionResult Create(Usuario usuario, HttpPostedFileBase foto)
         {
             string jsonData = JsonConvert.SerializeObject(usuario);
-            
+
             Usuario deserializeUsuario = JsonConvert.DeserializeObject<Usuario>(jsonData);
+
+            try
+            {
+                string fileName = "";
+                string contentType = "";
+                string path = "";
+                if (foto != null && foto.ContentLength > 0)
+                {
+                    fileName = System.IO.Path.GetFileName(foto.FileName);
+                    contentType = foto.ContentType;
+                    path = System.Configuration.ConfigurationManager.AppSettings["PathFiles"] + "\\Usuarios\\" + fileName;
+                    foto.SaveAs(path);
+                    deserializeUsuario.Imagem = fileName;
+                }
+            }
+            catch
+            {
+                ViewBag.MessageFoto = "Não foi possível salvar a foto";
+            }
             if (ModelState.IsValid)
             {
+
                 db.Usuarios.Add(deserializeUsuario);
                 db.SaveChanges();
                 return RedirectToAction("Create");
